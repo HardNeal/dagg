aac51835ba3525772bb1ae97624cc9bf2fc06926
Pr Ulan <b.a.w@bkru>
2017-05-10 19:08:12 +0600

    NEW

--- db/schema.rb
+++ db/schema.rb
@@ -12,37 +12,37 @@
 
 ActiveRecord::Schema.define(version: 20170505144002) do
 
-  create_table "active_admin_comments", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+  create_table "active_admin_comments", force: :cascade do |t|
     t.string   "namespace"
-    t.text     "body",          limit: 65535
-    t.string   "resource_id",                 null: false
-    t.string   "resource_type",               null: false
+    t.text     "body"
+    t.string   "resource_id",   null: false
+    t.string   "resource_type", null: false
     t.string   "author_type"
     t.integer  "author_id"
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.index ["author_type", "author_id"], name: "index_active_admin_comments_on_author_type_and_author_id", using: :btree
-    t.index ["namespace"], name: "index_active_admin_comments_on_namespace", using: :btree
-    t.index ["resource_type", "resource_id"], name: "index_active_admin_comments_on_resource_type_and_resource_id", using: :btree
+    t.index ["author_type", "author_id"], name: "index_active_admin_comments_on_author_type_and_author_id"
+    t.index ["namespace"], name: "index_active_admin_comments_on_namespace"
+    t.index ["resource_type", "resource_id"], name: "index_active_admin_comments_on_resource_type_and_resource_id"
   end
 
-  create_table "points", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
-    t.float    "longtitude", limit: 24
-    t.float    "latitude",   limit: 24
+  create_table "points", force: :cascade do |t|
+    t.float    "longtitude"
+    t.float    "latitude"
     t.integer  "tracker_id"
-    t.datetime "created_at",               null: false
-    t.datetime "updated_at",               null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
     t.datetime "point_time"
-    t.text     "message",    limit: 65535
-    t.index ["tracker_id"], name: "index_points_on_tracker_id", using: :btree
+    t.text     "message"
+    t.index ["tracker_id"], name: "index_points_on_tracker_id"
   end
 
-  create_table "protocols", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+  create_table "protocols", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
 
-  create_table "trackers", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+  create_table "trackers", force: :cascade do |t|
     t.string   "imei_code"
     t.string   "phone"
     t.datetime "created_at",                  null: false
@@ -51,32 +51,29 @@ ActiveRecord::Schema.define(version: 20170505144002) do
     t.string   "title"
     t.integer  "protocol_id"
     t.boolean  "active",      default: false
-    t.index ["protocol_id"], name: "index_trackers_on_protocol_id", using: :btree
-    t.index ["user_id"], name: "index_trackers_on_user_id", using: :btree
+    t.index ["protocol_id"], name: "index_trackers_on_protocol_id"
+    t.index ["user_id"], name: "index_trackers_on_user_id"
   end
 
-  create_table "users", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+  create_table "users", force: :cascade do |t|
     t.string   "phone_number"
-    t.datetime "created_at",                                          null: false
-    t.datetime "updated_at",                                          null: false
-    t.string   "email",                                default: "",   null: false
-    t.string   "encrypted_password",                   default: "",   null: false
+    t.datetime "created_at",                            null: false
+    t.datetime "updated_at",                            null: false
+    t.string   "email",                  default: "",   null: false
+    t.string   "encrypted_password",     default: "",   null: false
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.integer  "sign_in_count",                        default: 0,    null: false
+    t.integer  "sign_in_count",          default: 0,    null: false
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
     t.string   "current_sign_in_ip"
     t.string   "last_sign_in_ip"
-    t.boolean  "admin",                                default: true
-    t.text     "information",            limit: 65535
-    t.boolean  "active",                               default: true
-    t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
-    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
+    t.boolean  "admin",                  default: true
+    t.text     "information"
+    t.boolean  "active",                 default: true
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
-  add_foreign_key "points", "trackers"
-  add_foreign_key "trackers", "protocols"
-  add_foreign_key "trackers", "users"
 end
