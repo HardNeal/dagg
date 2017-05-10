370cd4f1564ead553d65dfad32553599cb782595
Pr Ulan <b.a.w@bkru>
2017-05-10 19:16:32 +0600

    saddasd

---
+++ tracker-server/start.rb
@@ -0,0 +1,29 @@
+ require 'dat-tcp'
+class Worker
+  
+  include DatTCP::Worker
+
+  def work!(socket)
+  	while @message = socket.gets
+     # @message = socket.read
+     if @message.chomp == ""
+      socket.puts 'Сообщение пустое'
+     else
+     	puts "Client : #{@message.chomp}"
+     	socket.puts "Данные принял"
+	   end
+	    break if @message =~ /^\s*$/ 
+	    end 
+     socket.write('Связь прервана!')
+  ensure
+    socket.close
+  end
+end
+
+
+
+server = DatTCP::Server.new(Worker)
+server.listen('localhost', 2000)
+server.start.join
+s = Worker.new
+s.work!
